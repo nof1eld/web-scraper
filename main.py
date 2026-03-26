fa95443792fd42c4c596b69d78da07306f855840
nofield <imadeddine799@gmail.com>
2026-03-26 17:24:03 +0100

    feat: dedicated getHTML function

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
 
-# get r object that contains html
-r = requests.get('https://ai-house-website.vercel.app/')
-# get the html from r object
-htmlCode = r.text
+def getHTML(url):
+    # Get the response object that contains html
+    response = requests.get(url)
+    # return the html from response
+    return response.text
 
 # parse and print html
 soup = BeautifulSoup(htmlCode, 'html.parser')
