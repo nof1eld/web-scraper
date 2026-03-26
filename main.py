841912c064020443904632d238367c05e6e1bc18
nofield <imadeddine799@gmail.com>
2026-03-26 17:13:48 +0100

    feat: add comments

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
 
+# get r object that contains html
 r = requests.get('https://ai-house-website.vercel.app/')
+# get the html from r object
 htmlCode = r.text
 
+# parse and print html
 soup = BeautifulSoup(htmlCode, 'html.parser')
 print(soup.prettify())
