194d05d547082281f327704cac91ec9f857502c2
mendas-cpu <omarmendas2018@gmail.com>
2026-03-27 21:27:01 +0100

    csv file to store data

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import csv
 from bs4 import BeautifulSoup
 import requests
 import json
+from dotenv import load_dotenv
 from os import getenv
 import google.genai as genai
 from google.genai import types
 
+load_dotenv()
 def getParsedHTML(url):
     # Get the response object that contains html
     response = requests.get(url)
@@ -13,7 +15,7 @@ def getParsedHTML(url):
     return BeautifulSoup(response.text, 'html.parser')
 
 def getSchemaJSON(html):
-        system_instruction = (
+        systemInstruction = (
             "Extract a scraping schema from the HTML.\n"
             "Return ONLY valid JSON in this format:\n"
             '{"row_selector":"...","fields":{"name":"selector"}}\n'
@@ -28,22 +30,44 @@ def getSchemaJSON(html):
             "- Return ONLY raw JSON.\n"
             "- Do NOT use markdown.\n"
             "- Do NOT wrap in ```json.\n"
+            #added these prompts to avoid returning pseudo-elements that BS don't support
+            "- Do NOT use pseudo-elements like ::text, ::attr, ::before, ::after\n"
+            "- Selectors must be valid CSS for BeautifulSoup/soupsieve\n"
         )
         try:
-            client = genai.Client(api_key="")
+            client = genai.Client(api_key=getenv("API_KEY"))
             response = client.models.generate_content(
                     model="gemini-2.5-flash",
                     config=types.GenerateContentConfig(
-                        system_instruction=system_instruction),
+                        system_instruction=systemInstruction),
                     contents=str(html)
                 ).text
             return json.loads(response)
         except Exception as e:
             print("An error occurred:", str(e))
 
-parsedHTML = getParsedHTML('https://books.toscrape.com/')
-schemaJSON = getSchemaJSON(parsedHTML)
 
-print(schemaJSON)
+#new function to scrapedata from parsedhtml
+def scrapeData(html, schema):
+    rows = html.select(schema["row_selector"])
+    scrapedData = []
+    for row in rows:
+        item = {}
+        for f, s in schema["fields"].items():
+            element = row.select_one(s)
+            item[f] = element.get_text(strip=True)if element else None
+        scrapedData.append(item)
+    return scrapedData
+parsedHTML = getParsedHTML('https://quotes.toscrape.com/')
+schemaJSON = getSchemaJSON(parsedHTML)
+scrapedData = scrapeData(parsedHTML, schemaJSON)
+with open("scrapedInfo.csv", "w", newline="", encoding="utf-8") as f:
+    writer = csv.DictWriter(f, fieldnames=scrapedData[0].keys())
+    writer.writeheader()
+    writer.writerows(scrapedData)
+print(scrapedData)
+
+
+
 
 
